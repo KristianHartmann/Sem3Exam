c5ad049a21801e352ad60ddbd4be5630e801dbaa
KristianHartmann <krha998@yahoo.dk>
2023-01-15 22:33:42 +0100

    added x-acess-token header in corsfiler

--- src/main/java/utils/SetupTestUsers.java
+++ src/main/java/utils/SetupTestUsers.java
@@ -1,10 +1,6 @@
 package utils;
 
 
-import entities.Boat;
-import entities.Cityinfo;
-import entities.Harbour;
-import entities.Owner;
 import security.entities.Role;
 import security.entities.User;
 
@@ -30,12 +26,6 @@ public class SetupTestUsers {
     User user = new User("user", "test1");
     User admin = new User("admin", "test2");
     User both = new User("user_admin", "test3");
-    Cityinfo cityinfo = new Cityinfo(2100, "name");
-    Harbour harbour = new Harbour("Harbour Name", "Harbour Address", 150);
-    Boat boat = new Boat("Brand", "Model", "Name", "Image URL");
-    Owner owner1 = new Owner("Phone1", "Address1");
-    Owner owner2 = new Owner("Phone2", "Address2");
-    Set<Owner> ownerSet = new LinkedHashSet<>();
 
     if(admin.getUserPass().equals("test")||user.getUserPass().equals("test")||both.getUserPass().equals("test"))
       throw new UnsupportedOperationException("You have not changed the passwords");
@@ -43,22 +33,10 @@ public class SetupTestUsers {
     em.getTransaction().begin();
     Role userRole = new Role("user");
     Role adminRole = new Role("admin");
-    harbour.setCityinfoIdcityinfo(cityinfo);
-    boat.setHarbourIdharbour(harbour);
-    owner1.setCityinfoIdcityinfo(cityinfo);
-    owner2.setCityinfoIdcityinfo(cityinfo);
-    ownerSet.add(owner1);
-    ownerSet.add(owner2);
-    boat.setOwners(ownerSet);
     user.addRole(userRole);
     admin.addRole(adminRole);
     both.addRole(userRole);
     both.addRole(adminRole);
-    em.persist(cityinfo);
-    em.persist(harbour);
-    em.persist(owner1);
-    em.persist(owner2);
-    em.persist(boat);
     em.persist(userRole);
     em.persist(adminRole);
     em.persist(user);
